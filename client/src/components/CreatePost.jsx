20b5e86545806d2007f41a7cc29bdb3b9700bb11
nour-ha123 <hanafi.nour11@gmail.com>
2026-04-19 17:53:18 +0100

    exredux

--- client/src/components/CreatePost.jsx
+++ client/src/components/CreatePost.jsx
@@ -1,30 +1,59 @@
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { addpost } from "../redux/postsreducer";
+import { useNavigate } from "react-router-dom";
+
 function CreatePost() {
+  const dispatch = useDispatch();
+  const nav = useNavigate();
+
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+  const [imageUrl, setImageUrl] = useState("");
+
+  const add = (e) => {
+    e.preventDefault();
+
+    dispatch(
+      addpost({
+        title: title,
+        content: content,
+        imageUrl: imageUrl,
+      })
+    );
+    nav("/");
+  };
+
   return (
     <div className="blog-list-item">
       <h1 className="New-Post-Title">New Post</h1>
 
-      <form className="New-Post-Form">
+      <form className="New-Post-Form" onSubmit={add}>
+        
         <input
-          id="title"
           type="text"
           className="search"
           placeholder="Title"
           required
           minLength="3"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
         />
 
         <textarea
-          id="content"
-          className="post "
+          className="post"
           placeholder="Content..."
           required
+          value={content}
+          onChange={(e) => setContent(e.target.value)}
         />
 
         <textarea
-          id="image"
-          className="post "
+          className="post"
           placeholder="Image..."
           required
+          value={imageUrl}
+          onChange={(e) => setImageUrl(e.target.value)}
         />
 
         <button className="btn btn-secondary searchButton" type="submit">
--- client/src/components/PostDetails.jsx
+++ client/src/components/PostDetails.jsx
@@ -1,14 +1,42 @@
-const PostDetails = () => {
+import { useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
+
+
+const PostDetails = ({ el }) => {
+  const dispatch = useDispatch();
+  const nav = useNavigate();
+
+  if (!el) return null;
+
   return (
     <div className="blog-list-item">
-      <button> delete </button>
 
-      <button> edit </button>
+      <button onClick={() => dispatch(deletePost(el._id))}>
+        delete
+      </button>
 
-      <div className="blog-list-item-title"></div>
-      <div className="blog-list-item-byline">a day ago</div>
+      <button
+        onClick={() => nav("/create", { state: { post: el } })}
+      >
+        edit
+      </button>
+
+      <div className="blog-list-item-title">
+        {el.title}
+      </div>
+
+      <div className="blog-list-item-byline">
+        {el.content}
+      </div>
+
       <div className="blog-list-item-lede">
-        <img  alt="no content" />
+        <img
+          src={el.imageUrl}
+          alt="no content"
+          onClick={() =>
+            nav("/onepost", { state: { el } })
+          }
+        />
       </div>
     </div>
   );
